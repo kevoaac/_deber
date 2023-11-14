24585112e75fafbea1ee9f0f87cdbfa29052def9
kevoaac <130163890+kevoaac@users.noreply.github.com>
2023-11-13 21:53:41 -0500

    Primer Commit

---
+++ src/main/java/com/uce/edu/DeberApplication.java
@@ -0,0 +1,21 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeberApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo");
+		System.out.println("Código para el commit de inicialización del proyecto");
+		
+	}
+
+}
